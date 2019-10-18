b93595a3532a8f2ddc1eacfca4b48741ec47f333
MikoTheAble <fuksior99775533@gmail.com>
2019-10-18 07:35:22 +0200

    Dodaj pliki projektów.

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(new DateModel());
+        }
+        public IActionResult JSON()
+        {
+            return Json(new { id = "1", text = "hello world" });
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult Redirection()
+        {
+            return View();
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
